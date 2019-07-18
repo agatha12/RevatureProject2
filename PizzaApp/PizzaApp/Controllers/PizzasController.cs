4314034f66df2f66323e22bde8335afcfbd0a865
gaelblanchard <gaelblanchard@live.com>
2019-07-18 15:51:16 -0400

    Scaffolded Controllers and Views

---
+++ PizzaApp/PizzaApp/Controllers/PizzasController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PizzaApp.Models;
+using PizzaEntities;
+
+namespace PizzaApp.Controllers
+{
+    public class PizzasController : Controller
+    {
+        private readonly PizzaAppContext _context;
+
+        public PizzasController(PizzaAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pizzas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pizza.ToListAsync());
+        }
+
+        // GET: Pizzas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pizza = await _context.Pizza
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            return View(pizza);
+        }
+
+        // GET: Pizzas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pizzas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,OrderId,size,sauce,crust")] Pizza pizza)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pizza);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pizza);
+        }
+
+        // GET: Pizzas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pizza = await _context.Pizza.FindAsync(id);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+            return View(pizza);
+        }
+
+        // POST: Pizzas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,OrderId,size,sauce,crust")] Pizza pizza)
+        {
+            if (id != pizza.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pizza);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PizzaExists(pizza.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pizza);
+        }
+
+        // GET: Pizzas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pizza = await _context.Pizza
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            return View(pizza);
+        }
+
+        // POST: Pizzas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pizza = await _context.Pizza.FindAsync(id);
+            _context.Pizza.Remove(pizza);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PizzaExists(int id)
+        {
+            return _context.Pizza.Any(e => e.id == id);
+        }
+    }
+}
---
+++ PizzaApp/PizzaApp/Data/PizzaAppContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PizzaEntities;
+
+namespace PizzaApp.Models
+{
+    public class PizzaAppContext : DbContext
+    {
+        public PizzaAppContext (DbContextOptions<PizzaAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PizzaEntities.Customer> Customer { get; set; }
+
+        public DbSet<PizzaEntities.Order> Order { get; set; }
+
+        public DbSet<PizzaEntities.Pizza> Pizza { get; set; }
+
+        public DbSet<PizzaEntities.PizzaTopping> PizzaTopping { get; set; }
+
+        public DbSet<PizzaEntities.Topping> Topping { get; set; }
+    }
+}
