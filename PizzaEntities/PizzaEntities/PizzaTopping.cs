a0daacabf917073ff4330f2e462742ef2a58771d
agatha12 <agkiecun@gmail.com>
2019-07-19 11:31:56 -0400

    all entities include data contract and customer controller is modified but not complete

--- PizzaEntities/PizzaEntities/PizzaTopping.cs
+++ PizzaEntities/PizzaEntities/PizzaTopping.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace PizzaEntities
 {
+    [DataContract]
     public class PizzaTopping
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Key]
+        [DataMember]
         public int id { get; set; }
         [Required]
+        [DataMember]
         public int pizzaId { get; set; }
         [Required]
+        [DataMember]
         public int toppingId { get; set; }
     }
 }
--- PizzaEntities/PizzaEntities/Topping.cs
+++ PizzaEntities/PizzaEntities/Topping.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace PizzaEntities
 {
-
+    [DataContract]
     public class Topping
     {
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         [Key]
+        [DataMember]
         public int id { get; set; }
         [Required]
+        [DataMember]
         public string name { get; set; }
         [Required]
+        [DataMember]
         public decimal price { get; set; }
 
     }
