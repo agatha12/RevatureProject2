02d3b4388735390043cfe79fe87652e673a31985
gaelblanchard <gaelblanchard@live.com>
2019-07-19 11:56:51 -0400

    Added API

---
+++ PizzaAPI/Controllers/PizzasController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaApp.Models;
+using PizzaEntities;
+
+namespace PizzaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzasController : ControllerBase
+    {
+        private readonly PizzaAppContext _context;
+
+        public PizzasController(PizzaAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pizzas
+        [HttpGet]
+        public IEnumerable<Pizza> GetPizza()
+        {
+            return _context.Pizza;
+        }
+
+        // GET: api/Pizzas/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPizza([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var pizza = await _context.Pizza.FindAsync(id);
+
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pizza);
+        }
+
+        // PUT: api/Pizzas/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPizza([FromRoute] int id, [FromBody] Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != pizza.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pizza).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PizzaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pizzas
+        [HttpPost]
+        public async Task<IActionResult> PostPizza([FromBody] Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Pizza.Add(pizza);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PizzaExists(pizza.id))
+                {
+                    return new StatusCodeResult(StatusCodes.Status409Conflict);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetPizza", new { id = pizza.id }, pizza);
+        }
+
+        // DELETE: api/Pizzas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePizza([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var pizza = await _context.Pizza.FindAsync(id);
+            if (pizza == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pizza.Remove(pizza);
+            await _context.SaveChangesAsync();
+
+            return Ok(pizza);
+        }
+
+        private bool PizzaExists(int id)
+        {
+            return _context.Pizza.Any(e => e.id == id);
+        }
+    }
+}
---
+++ PizzaAPI/Models/PizzaContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PizzaEntities;
+
+namespace PizzaApp.Models
+{
+    public class PizzaAppContext : DbContext
+    {
+        public PizzaAppContext(DbContextOptions<PizzaAppContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Pizza> Pizza { get; set; }
+        public DbSet<PizzaTopping> PizzaToppings { get; set; }
+        public DbSet<Topping> Toppings { get; set; }
+
+    }
+}
+
